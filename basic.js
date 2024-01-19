90b501f777666491e6a8287dfabf33a5ffa53fb5
mohanrajyadav <155557180+mohanrajyadav@users.noreply.github.com>
2024-01-19 08:16:24 +0000

    like string type

---
+++ basic.js
@@ -0,0 +1,11 @@
+const username = new String('mohanrajyadav')
+console.log(username[0]);
+console.log(username.length);
+console.log(username.charAt(8));
+console.log(username.substring(0 , 6));
+let email = "   mohan@rajyadav"
+console.log(email.trim());
+
+let emailtwo = "mohanraj@fatehpur"
+console.log(emailtwo.replace('mohan' ,'fatehpur'));
+console.log(emailtwo.toUpperCase());
